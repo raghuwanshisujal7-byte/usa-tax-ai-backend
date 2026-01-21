08675c74bdf4d0657d2ac7ef2e92c8cadff90506
raghuwanshisujal7-byte <raghuwanshisujal7@gmail.com>
2026-01-22 01:49:13 +0530

    Fix IRS deductions source

---
+++ src/irs/deduction.js
@@ -0,0 +1,10 @@
+module.exports = {
+  basicDeductions: [
+    "Standard Deduction (2024): $13,850 (Single), $27,700 (Married Filing Jointly)",
+    "401(k) and IRA contributions reduce taxable income",
+    "Health Savings Account (HSA) contributions are tax-deductible",
+    "Student loan interest deduction up to $2,500",
+    "Mortgage interest deduction on primary residence",
+    "Charitable contributions deduction"
+  ]
+};
