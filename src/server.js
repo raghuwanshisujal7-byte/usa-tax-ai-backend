4f42b695731ba8d4563124722eec6895788478ad
raghuwanshisujal7-byte <raghuwanshisujal7@gmail.com>
2026-01-21 22:26:22 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,9 +1,7 @@
 const app = require("./app");
 
-// Render ya local se PORT aayega
 const PORT = process.env.PORT || 3000;
 
-// Server start
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
