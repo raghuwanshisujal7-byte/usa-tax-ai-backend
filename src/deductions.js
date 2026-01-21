f5aff4878d90c141aa6df9d056651e9f5201a203
raghuwanshisujal7-byte <raghuwanshisujal7@gmail.com>
2026-01-21 20:29:35 +0530

    Add IRS deductions data

---
+++ src/deductions.js
@@ -0,0 +1,9 @@
+const basicDeductions = [
+  "Standard Deduction (2024): $13,850 (Single), $27,700 (Married Filing Jointly)",
+  "401(k) & IRA contributions reduce taxable income",
+  "Health Savings Account (HSA) contributions are tax-deductible",
+  "Student loan interest deduction up to $2,500",
+  "Mortgage interest deduction on primary residence"
+];
+
+module.exports = { basicDeductions };
