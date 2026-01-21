7c349e5687b169db67cf4dc3bb07d5f2b70d6231
raghuwanshisujal7-byte <raghuwanshisujal7@gmail.com>
2026-01-21 20:30:48 +0530

    Add IRS credits data

---
+++ src/irs/credits.js
@@ -0,0 +1,9 @@
+const basicCredits = [
+  "Child Tax Credit up to $2,000 per qualifying child",
+  "Earned Income Tax Credit (EITC) for low to moderate income earners",
+  "American Opportunity Credit for college expenses",
+  "Lifetime Learning Credit for education",
+  "Energy Efficient Home Improvement Credit"
+];
+
+module.exports = { basicCredits };
